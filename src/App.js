b6c3d08132f3fb049b31376dc2192531d433f822
swamina5 <swamina5@purdue.edu>
2022-11-10 11:19:10 -0500

    Added icons, plus a sidebar

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import ReactDOM from "react-dom";
 
@@ -10,29 +9,6 @@ function App() {
       document.getElementById("root")
       );
     }
-
- 
-
-
-
-
-
-    {/* // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div> */}
 
 
 export default App;
--- src/Pages/Admin.js
+++ src/Pages/Admin.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
- 
+import './Admin.css';
+
 class Admin extends Component {
+
   render() {
-    return (
-        <div>
-          <h1>Simple SPA</h1>
-          <ul className="header">
-            <li><a href="/">Home</a></li>
-            <li><a href="/stuff">Stuff</a></li>
-            <li><a href="/contact">Contact</a></li>
-          </ul>
-          <div className="content">
-             
-          </div>
+    const divStyle = {
+      position: "absolute",
+      left: "-80px",
+      transition: "0.3s",
+      padding: "15px",
+      width: "100px",
+      text_decoration: "none",
+      font_size: "20px",
+      color: "white",
+      border_radius: "0 5px 5px 0"
+    };
+
+    return (      
+      <body>
+
+        <div id="mySidenav" class="sidenav">
+          <a href="#" id="about">About</a>
+          <a href="#" id="blog">Blog</a>
+          <a href="#" id="projects">Projects</a>
+          <a href="#" id="contact">Contact</a>
         </div>
-    );
+
+        <div style={divStyle}>
+          <h2>Hoverable Sidenav Buttons</h2>
+          <p>Hover over the buttons in the left side navigation to open them.</p>
+        </div>
+          
+      </body>
+
+      );
+      
   }
 }
- 
 export default Admin;
